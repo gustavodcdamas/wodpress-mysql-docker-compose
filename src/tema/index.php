5cca94d66e063bdd7775f4a5b067f39b0b26103a
gustavodcdamas <gustavodcdamas@gmail.com>
2025-02-07 11:08:31 -0300

    commit inicial

---
+++ src/tema/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Portfolio home template with post featured images
+ * Slug: twentytwentyfour/template-home-portfolio
+ * Template Types: front-page, home
+ * Viewport width: 1400
+ * Inserter: no
+ */
+?>
+
+
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+
+		<meta name="viewport" content="initial-scale=1, width=device-width">
+		<link rel="stylesheet"  src="./assets/src/css/style.css" />
+		<script src="./assets/src/css/main.js"></script>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira Code:wght@400;500;600;700&display=swap" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+		<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" />
+		<meta charset="UTF-8">
+		<title>Document</title>
+
+	</head>
+	<body>
+		
+	</body>
+	</html>
